6a889a14457c7f9208d54166073792033e3abc5a
Arshad Husain <arshad.husain1@gmail.com>
2022-02-14 02:50:33 -0700

    Housekeeping

--- src/portfolio.js
+++ src/portfolio.js
@@ -341,8 +341,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-3243454077",
-  email_address: "saadpasta70@gmail.com"
+  number: "",
+  email_address: ""
 };
 
 // Twitter Section

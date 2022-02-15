f2d65fcdc569ce5d5f5047e46f216f30a91f3109
Arshad Husain <arshad.husain1@gmail.com>
2022-02-14 20:07:44 -0700

    Add Github repos and changed github contact color

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -14,7 +14,7 @@ export default function Contact() {
       <div className="main contact-margin-top" id="contact">
         <div className="contact-div-main">
           <div className="contact-header">
-            <h1 className="heading contact-title">{contactInfo.title}</h1>
+            <h1 className={isDark ? "dark-mode heading contact-title" : "heading contact-title"}>{contactInfo.title}</h1>
             <p
               className={
                 isDark
